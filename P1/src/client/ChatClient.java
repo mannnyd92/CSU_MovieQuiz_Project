bc297f92388d12dd005b4af20702eb7dcdab544d
kvandiem <kvandiem@montpelier.cs.colostate.edu>
2014-10-12 12:54:50 -0600

    Changes for E51(a)(b) implemented.  --Kurt

--- P1/src/client/ChatClient.java
+++ P1/src/client/ChatClient.java
@@ -26,7 +26,8 @@ public class ChatClient extends AbstractClient
    * the display method in the client.
    */
   ChatIF clientUI; 
-
+  String loginID;
+  
   
   //Constructors ****************************************************
   
@@ -38,12 +39,15 @@ public class ChatClient extends AbstractClient
    * @param clientUI The interface type variable.
    */
   
-  public ChatClient(String host, int port, ChatIF clientUI) 
+  public ChatClient(String login, String host, int port, ChatIF clientUI) 
     throws IOException 
   {
     super(host, port); //Call the superclass constructor
     this.clientUI = clientUI;
+    this.loginID = login;
     openConnection();
+    String loginMsg = "#login <" + login + ">";
+    this.sendToServer(loginMsg);
   }
 
   
