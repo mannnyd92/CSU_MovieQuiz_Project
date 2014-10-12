8ee1b0dc24c6a45a7c9aa292f554d54f968756c2
mannnyd <mannnyd@montgomery.cs.colostate.edu>
2014-10-12 13:07:18 -0600

    Part e50 (c) pushing but still need to test

--- P1/src/ServerConsole.java
+++ P1/src/ServerConsole.java
@@ -42,15 +42,76 @@ public class ServerConsole implements ChatIF{
 	  }
 	
 	public void handleMessageFromServerUI(String message){
+		if(message.charAt(0) == '#'){
+			String tmpmes = message.substring(1,message.length());
+			String param = "";
+			String chkmes = "";
+			boolean flag = false;
+			if(tmpmes.length() > 6){
+				chkmes = tmpmes.substring(0,7);
+				if(tmpmes.length() > 6){
+							
+							chkmes = tmpmes.substring(0,7);
+								
+							
+							if(chkmes.equals("setport")){
+								if( tmpmes.length() > 8){
+									if(tmpmes.charAt(7) != ' '){
+										System.out.println("A space is required after the command!");
+									}
+									else{
+									param = tmpmes.substring(8, tmpmes.length());
+									tmpmes = "setport";
+									flag = true;
+									}
+								}else{System.out.println("Please give parameter after command!");}
+							}
+						}
+						
+						switch (tmpmes){
+							case "quit":	try{server.close();}catch(Exception e){}
+											break;
+					
+											
+							case "setport":	if(!server.isListening() && flag){
+												try{
+													int port = Integer.valueOf(param);
+													server.setPort(port);
+													flag = false;
+												}catch(Exception e){}
+											}else if(server.isListening() && flag){
+												System.out.println("Server already up! Stop server to change port");}
+											break;	
+											
+											
+							case "getport":	System.out.println(server.getPort());
+											break;
+								
+							case "stop": 	server.stopListening();
+											break;
+											
+							case "start": 	if(!server.isListening()){
+												try{server.listen();}catch(Exception e){}
+											}
+											else{
+												System.out.println("Server already up!");
+											}
+											break;
+							default:		System.out.println("# Requires that it is followed by a command and a parameter!");
+							
+						}
+							  
+					  }
+		
+					  else{
 		
 		
 		
-		
-		
+						
 		String serverMessage = "SERVER MSG> " + message;
 		server.sendToAllClients(serverMessage);
 		display(message);
-	}
+			}}}
 	  
 	@Override
 	public void display(String message) {
