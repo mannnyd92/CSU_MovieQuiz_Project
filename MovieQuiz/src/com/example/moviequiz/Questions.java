b72caa400804ff847798e7ead04683d0569abd88
Kurt M. VanDiemen <kvandiem@saint-paul.cs.colostate.edu>
2014-12-04 16:58:10 -0700

    Modified the file system and fixed the quiz.

--- MovieQuiz/src/com/example/moviequiz/Questions.java
+++ MovieQuiz/src/com/example/moviequiz/Questions.java
@@ -2,13 +2,16 @@ package com.example.moviequiz;
 
 
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
+
+import android.content.res.AssetManager;
+
 
 public class Questions {
 	 String que;
@@ -64,51 +67,55 @@ public class Questions {
 		selected = x;
 	}
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	public void readQuestions(String file){
-		File input = new File(file);
+	public void readQuestions(AssetManager a){
+		
+		InputStream input = null;
+		try {
+			input = a.open("P4Trivia");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	
 		int count = 0;
-		try{
-			Scanner scan = new Scanner(input);
-		
-			System.out.println("test");
-			while(scan.hasNextLine()){
-				
-				que = scan.nextLine();
-				opt1 = scan.nextLine();
-				opt2 = scan.nextLine();
-				opt3 = scan.nextLine();
-				opt4 = scan.nextLine();
-				
-				if(opt1.contains("&")){
-					opt1 = opt1.substring(1);
-					correct = opt1;
-				}
-				
-				if(opt2.contains("&")){
-					opt2 = opt2.substring(1);
-					correct = opt2;
-				}
-				if(opt3.contains("&")){
-					opt3 = opt3.substring(1);
-					correct = opt3;
-				}
-				if(opt4.contains("&")){
-					opt4 = opt4.substring(1);
-					correct = opt4;
-				}
+		Scanner scan = new Scanner(input);
 
-				
-				map.put(count, que + "|" + opt1 + "|" + opt2 + "|" + opt3 + "|" + opt4 + "|" + correct);
 
-				count++;
-				
-				
-					
-				
+		while(scan.hasNextLine()){
+			
+			que = scan.nextLine();
+			opt1 = scan.nextLine();
+			opt2 = scan.nextLine();
+			opt3 = scan.nextLine();
+			opt4 = scan.nextLine();
+			
+			if(opt1.contains("&")){
+				opt1 = opt1.substring(1);
+				correct = opt1;
 			}
-			//System.out.println(map);
-		}catch(IOException e){System.out.println("exception");}
+			
+			if(opt2.contains("&")){
+				opt2 = opt2.substring(1);
+				correct = opt2;
+			}
+			if(opt3.contains("&")){
+				opt3 = opt3.substring(1);
+				correct = opt3;
+			}
+			if(opt4.contains("&")){
+				opt4 = opt4.substring(1);
+				correct = opt4;
+			}
+
+			
+			map.put(count, que + "|" + opt1 + "|" + opt2 + "|" + opt3 + "|" + opt4 + "|" + correct);
+
+			count++;
+			
+			
+				
+			
+		}
 		
 	}
 	public void getRandom(){
@@ -120,7 +127,6 @@ public class Questions {
 					random.add(rando);
 				}
 			}
-		System.out.println(random);
 	}
 	public void makeQuestions(){
 		
@@ -163,7 +169,7 @@ public class Questions {
 		opt3 = single[3];
 		opt4 = single[4];
 		correct = single[5];
-		Questions que4 = new Questions(que, opt1, opt2, opt3, opt4, correct);
+		que4 = new Questions(que, opt1, opt2, opt3, opt4, correct);
 		
 		quest = map.get(random.get(4));
 		single = quest.split("\\|");
@@ -173,7 +179,7 @@ public class Questions {
 		opt3 = single[3];
 		opt4 = single[4];
 		correct = single[5];
-		Questions que5 = new Questions(que, opt1, opt2, opt3, opt4, correct);
+		que5 = new Questions(que, opt1, opt2, opt3, opt4, correct);
 		
 		quest = map.get(random.get(5));
 		single = quest.split("\\|");
@@ -183,7 +189,7 @@ public class Questions {
 		opt3 = single[3];
 		opt4 = single[4];
 		correct = single[5];
-		Questions que6 = new Questions(que, opt1, opt2, opt3, opt4, correct);
+		que6 = new Questions(que, opt1, opt2, opt3, opt4, correct);
 		
 		quest = map.get(random.get(6));
 		single = quest.split("\\|");
@@ -193,7 +199,7 @@ public class Questions {
 		opt3 = single[3];
 		opt4 = single[4];
 		correct = single[5];
-		Questions que7 = new Questions(que, opt1, opt2, opt3, opt4, correct);
+		que7 = new Questions(que, opt1, opt2, opt3, opt4, correct);
 		
 		quest = map.get(random.get(7));
 		single = quest.split("\\|");
@@ -203,7 +209,7 @@ public class Questions {
 		opt3 = single[3];
 		opt4 = single[4];
 		correct = single[5];
-		Questions que8 = new Questions(que, opt1, opt2, opt3, opt4, correct);
+		que8 = new Questions(que, opt1, opt2, opt3, opt4, correct);
 		
 		quest = map.get(random.get(8));
 		single = quest.split("\\|");
@@ -213,7 +219,7 @@ public class Questions {
 		opt3 = single[3];
 		opt4 = single[4];
 		correct = single[5];
-		Questions que9 = new Questions(que, opt1, opt2, opt3, opt4, correct);
+		que9 = new Questions(que, opt1, opt2, opt3, opt4, correct);
 		
 		quest = map.get(random.get(9));
 		single = quest.split("\\|");
@@ -223,23 +229,13 @@ public class Questions {
 		opt3 = single[3];
 		opt4 = single[4];
 		correct = single[5];
-		Questions que10 = new Questions(que, opt1, opt2, opt3, opt4, correct);
+		que10 = new Questions(que, opt1, opt2, opt3, opt4, correct);
 		
-		System.out.println(que1 + "\n" + que2 + "\n" + que3 + "\n" + que4 + "\n" + que5 + "\n" + que6 + "\n" + que7 + "\n" + que8 + "\n" + que9 + "\n" + que10 );
 	}
 	public String toString(){
 		String out;
 		out = que + "\n" + opt1 + "\n" + opt2 + "\n" + opt3 + "\n" + opt4;
 		return out;
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Questions q = new Questions();
-		q.readQuestions("P4Trivia");
-		q.getRandom();
-		q.makeQuestions();
-
-		
 	}
 
 }
--- MovieQuiz/src/com/example/moviequiz/Quiz.java
+++ MovieQuiz/src/com/example/moviequiz/Quiz.java
@@ -1,20 +1,30 @@
 package com.example.moviequiz;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class Quiz extends Activity {
 
-	Questions q = new Questions();
+
+	ArrayList<Questions> questions = new ArrayList<Questions>();
+	int position = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
 		getQuestions();
+
 	}
 
 	@Override
@@ -24,9 +34,40 @@ public class Quiz extends Activity {
 		return true;
 	}
 	
+	protected void display(int p){
+		TextView quest = (TextView) findViewById(R.id.textView1);
+		quest.setText(questions.get(p).que);
+		RadioButton button1 = (RadioButton) findViewById(R.id.radio0);
+		button1.setText(questions.get(p).opt1);
+		RadioButton button2 = (RadioButton) findViewById(R.id.radio1);
+		button2.setText(questions.get(p).opt2);
+		RadioButton button3 = (RadioButton) findViewById(R.id.radio2);
+		button3.setText(questions.get(p).opt3);
+		RadioButton button4 = (RadioButton) findViewById(R.id.radio3);
+		button4.setText(questions.get(p).opt4);
+	}
+	
 	protected void getQuestions(){
+		Resources resources = getResources();
+		AssetManager file = resources.getAssets();
 		
-		
+		Questions q = new Questions();
+		q.readQuestions(file);
+		q.getRandom();
+		q.makeQuestions();
+
+		questions.add(q.que1);
+		questions.add(q.que2);
+		questions.add(q.que2);
+		questions.add(q.que3);
+		questions.add(q.que4);
+		questions.add(q.que5);
+		questions.add(q.que6);
+		questions.add(q.que7);
+		questions.add(q.que8);
+		questions.add(q.que9);
+		questions.add(q.que10);
+		display(position);
 	}
 	
 	public void next(View view){
